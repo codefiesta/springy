b5724db7f33d277667ffdccc3e6e1a4a342678af
Kevin McKee <kevin.mckee@procore.com>
2020-08-29 14:58:25 -0400

    Saving work

--- web/static/js/springy-sdk.js
+++ web/static/js/springy-sdk.js
@@ -2,20 +2,39 @@
 class Springy {
 
     constructor(config) {
-
-        this.callbacks = [];
-        this.isConnected = false;
-
         if (!window["WebSocket"]) {
             console.log('💩 Your browser does not support WebSockets.');
             return;
         }
-
-        this.ws = new WebSocket(config.databaseURL);
-        this.#addHandlers();
+        this._database = new Database(config);
     }
 
-    #addHandlers = () => {
+    // Returns our enum of events
+    static get events() {
+        return Object.freeze({
+            insert: "insert",
+            update: "update",
+            remove: "delete",
+            replace: "replace",
+        });
+    }
+
+    // Returns the app database object
+    get database() {
+        return this._database;
+    }
+}
+
+class Database {
+
+    constructor(config) {
+        this.isConnected = false;
+        this.references = new Map();
+        this.ws = new WebSocket(config.databaseURL);
+        this.#addSocketHandlers();
+    }
+
+    #addSocketHandlers = () => {
 
         let self = this;
 
@@ -30,38 +49,24 @@ class Springy {
         this.ws.onmessage = function (e) {
             try {
                 let message = JSON.parse(e.data);
-                self.#routeMessage(message);
+                self.#broadcast(message);
             } catch(err) {
                 console.error(err);
             }
         };
     };
 
-    watch = (path, eventType, callback) => {
-
-        let message = {
-            path: path,
-            action: "watch",
-            operation: eventType
-        };
-
-        if (callback) { this.callbacks.push(callback); }
-        let payload = JSON.stringify(message);
-        this.#send(payload);
-    };
-
-    /// Internal Message Handling
-
-    #routeMessage = (message) => {
-        // Handle the incoming message
-        for (const callback of this.callbacks) {
-            callback(message);
-        }
-    };
-
-    #send = (message) => {
+    /// Publishes a message out to the database
+    publish = (message) => {
         this.#queueMessage(() => {
             this.ws.send(message);
+        });
+    };
+
+    /// Broadcasts an incoming message to reference handlers
+    #broadcast = (message) => {
+        this.references.forEach((value, key) => {
+            value.notify(message);
         });
     };
 
@@ -71,8 +76,63 @@ class Springy {
         } else {
             let self = this;
             setTimeout(() => {
-               self.#queueMessage(callback);
+                self.#queueMessage(callback);
             }, 5);
         }
+    };
+
+    // Returns a data reference for the specified path
+    reference = (path) => {
+        if (this.references.has(path)) {
+            return this.references.get(path);
+        }
+        let reference = new DataReference(this, path);
+        this.references.set(path, reference);
+        return reference;
+    };
+}
+
+class DataReference {
+
+    constructor(database, path) {
+        this.database = database;
+        this.subscribers = [];
+        this.path = path;
+    }
+
+    child = (path) => {
+        return new DataReference(this.database, path);
+    }
+
+    // Watches a database reference
+    watch = (eventType, callback) => {
+
+        let message = {
+            path: this.path,
+            action: "watch",
+            operation: eventType
+        };
+
+        console.log(`👀 ${eventType}`);
+        if (callback) { this.subscribers.push(callback); }
+
+        const db = this.database;
+        if (db) {
+            let payload = JSON.stringify(message);
+            db.publish(payload);
+        }
+    };
+
+    notify = (message) => {
+        for (const subscriber of this.subscribers) {
+            subscriber(message);
+        }
+    }
+
+    // Writes data to this Database location.
+    // This will overwrite any data at this location and all child locations.
+    // Passing null for the new value is equivalent to calling remove()
+    set = (value, onComplete) => {
+
     };
 }
